26664eb8fef066bd09ba9bb7caefad87da924236
Ines Herminio <84570798+inesherminio@users.noreply.github.com>
2022-04-29 12:02:05 +0100

    Lab reviewed

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ hbs.registerPartials( __dirname + '/views/partials')
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+
 
 app.get('/beers', async (req, res) => {
  const beers = await punkAPI.getBeers();
@@ -29,6 +27,17 @@ app.get('/beers', async (req, res) => {
 app.get('/random-beers', async (req, res) => {
   const randomBeer = await punkAPI.getRandom();
   res.render('random-beers', {randomBeer});
+  // res.render('random-beers', {randomBeer}, {layout: false});
+  // The above line is what you would do if you didnt want the layout applying to this specific render
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// Just a small comment that we havent discussed in class yet, so no worries:
+// Routes should be written from the most specific to the most general ones
+// This is because, sometimes, some random unexplained errors might occur and the server may confuse "/"" with "/beers" because they both start the same
+// Unlikely, but may happen. So a best practice is to write the general ones at the end. In this case "/" is the last
