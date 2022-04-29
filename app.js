633bd0762f7cd70b7f6bc26b2d05eed685c874d6
Ines Herminio <84570798+inesherminio@users.noreply.github.com>
2022-04-29 11:32:48 +0100

    Begona

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname +'/views/partials')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-// Register the location for handlebars partials here:
 
-// ...
 
-// Add the route handlers here:
+// Adding the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', (req, res,next) => {
   res.render('index');
 });
+app.get("/beers", (req, res, next) => {
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+  //Getting beers from API
+  punkAPI.getBeers()
+  // response from database 'beers' placeholder
+  .then(beers =>{
+    // rendering the data to page with {beers} object
+   //console.log(`Response from DB: ${beers}`);
+   res.render("beers",{beers});
+  })
+  .catch(error=>{// =====> holds error callback
+    res.send(error)})
+});
+
+app.get("/random-beers", (req, res, next) => {
+  //getRandom beer using the defined method
+  punkAPI.getRandom()
+  .then(anyRandomBeer=>{
+//rendering and object to page beer variable
+// console.log(`A random beer: ${anyRandomBeer}`);
+    res.render('random', {random: anyRandomBeer})
+  })
+  .catch(error => {
+    console.log(error)
+  });
+});
+  
+app.listen(3500, () => console.log('🏃‍ on port 3500'));
+  
