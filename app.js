d24e2136dcfc98b1b7cc015738ccaf594a551503
Ines Herminio <84570798+inesherminio@users.noreply.github.com>
2022-04-29 11:15:20 +0100

    Alice's lab

--- app.js
+++ app.js
@@ -9,17 +9,59 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beerRandom => {
+      console.log(beerRandom);
+      let randomBeer = beerRandom[0];
+      res.render('randomBeer', randomBeer);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+// app.get('/beers', (req, res) => {
+//   let id = req.params.id;
+//   console.dir(req.params.id)
+//   punkAPI
+//     .getBeer(id)
+//     .then(beerSingle => {
+//       console.log(beerSingle);
+      
+//       res.render('singleBeer', beerSingle);
+      
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// });
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
